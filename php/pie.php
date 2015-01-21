fd12a1225f2f0edc89ff76193f06c72c01878e59
squall172011@gmail.com <squall172011@gmail.com@eba5fab6-e8af-8771-bccb-056eb82fb78f>
2015-01-21 14:38:48 +0000

    arreglo de google analyst y pie de pagina en php index

---
+++ php/pie.php
@@ -0,0 +1,19 @@
+<?php 
+echo '
+	<div id="pie">
+			<div id="enlaces">
+				<h3>Sitios Relacionados</h3>
+				<hr/>
+				<a href="http://www.museodeterque.com/" title="Museo de Terque">Museo de Terque</a></br>
+				<a href="http://www.alpujarradetapas.com/" title="Alpujarra de Tapas">Alpujarra de Tapas</a></br>
+				<a href="https://www.facebook.com/pages/Casas-Rurales-Luisi/727170950674635" title="Casas Rurales Luisi">Casas Rurales Luisi</a></br>
+			</div>
+			<div id="contacta">
+				<h3>Contacta con nosotros</h3>
+				<hr/>
+				<a>Telf: 622-112-446 </a></br>
+				<a>Direccion: Plaza del la Constituci&oacute;n Bentarique(Almer&iacute;a).</a>
+			</div>
+		</div>
+		';
+?>
